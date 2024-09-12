f1768d17047cc8917042a80fab1bddf70b1d53bd
gerardnicolas <77805536+gerardnicolas@users.noreply.github.com>
2024-09-12 21:14:47 +0800

    Update User API & Update User Page

---
+++ app/users/[id]/page.tsx
@@ -0,0 +1,24 @@
+import UserForm from '@/components/UserForm';
+import prisma from '@/prisma/db';
+import React from 'react';
+
+interface Props {
+  params: { id: string };
+}
+
+const EditUser = async ({ params }: Props) => {
+  const user = await prisma?.user.findUnique({
+    where: {
+      id: parseInt(params.id),
+    },
+  });
+
+  if (!user) {
+    return <p className="text-destructive">User not found!</p>;
+  }
+
+  user.password = '';
+  return <UserForm user={user} />;
+};
+
+export default EditUser;
