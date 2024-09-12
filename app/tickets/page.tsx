28e3475eb5863d7f9596d4534fa100a54f6ea284
gerardnicolas <77805536+gerardnicolas@users.noreply.github.com>
2024-09-12 12:47:28 +0800

    Sorting with Prisma

--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -17,8 +17,9 @@ const Tickets = async ({ searchParams }: { searchParams: SearchParams }) => {
   const pageSize = 10;
   const page = parseInt(searchParams.page) || 1;
 
-  const statuses = Object.values(Status);
+  const orderBy = searchParams.orderBy ? searchParams.orderBy : "createdAt"
 
+  const statuses = Object.values(Status);
   const status = statuses.includes(searchParams.status)
     ? searchParams.status
     : undefined;
@@ -38,6 +39,9 @@ const Tickets = async ({ searchParams }: { searchParams: SearchParams }) => {
   const ticketCount = await prisma.ticket.count({ where });
   const tickets = await prisma.ticket.findMany({
     where,
+    orderBy: {
+      [orderBy]: "desc",
+    },
     take: pageSize,
     skip: (page - 1) * pageSize,
   });
