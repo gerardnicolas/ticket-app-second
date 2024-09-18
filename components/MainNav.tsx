deb41d443fc39959d9b625153af6424db840f3c6
gerardnicolas <77805536+gerardnicolas@users.noreply.github.com>
2024-09-18 12:07:19 +0800

    Added use client in MainNavLinks

--- components/MainNav.tsx
+++ components/MainNav.tsx
@@ -1,17 +1,22 @@
-"use client"
-
 import Link from 'next/link';
 import React from 'react';
 import { ModeToggle } from './ModeToggle';
-import { Button } from './ui/button';
 import MainNavLinks from './MainNavLinks';
+import { getServerSession } from 'next-auth';
+import options from '@/app/api/auth/[...nextauth]/options';
 
-const MainNav = () => {
+const MainNav = async () => {
+  const session = await getServerSession(options);
+
   return (
     <div className="flex justify-between">
       <MainNavLinks />
       <div className="flex items-center gap-4">
-        <Button>Logout</Button>
+        {session ? (
+          <Link href="/api/auth/signout?callbackUrl=/">Logout</Link>
+        ) : (
+          <Link href="/api/auth/signin">Login</Link>
+        )}
         <ModeToggle />
       </div>
     </div>
