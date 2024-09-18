c0670af1eae8be29b7f5583823dcaf1f23a37cbd
gerardnicolas <77805536+gerardnicolas@users.noreply.github.com>
2024-09-18 18:54:44 +0800

    Protecting Pages

--- app/users/[id]/page.tsx
+++ app/users/[id]/page.tsx
@@ -1,12 +1,27 @@
 import UserForm from '@/components/UserForm';
 import prisma from '@/prisma/db';
 import React from 'react';
+import { getServerSession } from 'next-auth';
+import options from '@/app/api/auth/[...nextauth]/options';
 
 interface Props {
   params: { id: string };
 }
 
 const EditUser = async ({ params }: Props) => {
+  // Get the user's session
+  const session = await getServerSession(options);
+
+  // Ensure the user is an admin before allowing them to edit another user.
+  if (session?.user.role !== 'ADMIN') {
+    // If the user is not an admin, display an error message.
+    return (
+      <p className="text-destructive">
+        You must be an administrator to edit other users.
+      </p>
+    );
+  }
+
   const userId = parseInt(params.id, 10);
 
   if (isNaN(userId)) {
